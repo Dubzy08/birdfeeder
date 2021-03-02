a1adb1b1b1fd7d7064231539871302b53221f19e
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-03-02 16:16:44 -0500

    Update birdfeeder.php

--- birdfeeder/birdfeeder.php
+++ birdfeeder/birdfeeder.php
@@ -5,6 +5,6 @@
     </head>
     <body>
         <h1>Raspberry Pi - Surveillance Camera</h1>
-        <img src="stram.mjpg" width="640" height="480">
+        <img src="stream.mjpg" width="640" height="480">
     </body>
 </html>
