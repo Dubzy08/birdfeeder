a524f7705a7379c756c3f1179ea57bf33e281c4d
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-03-02 14:47:44 -0500

    Create birdfeeder.php

---
+++ birdfeeder.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>test Raspberry Pi - Surveillance Camera</title>
+    </head>
+    <body>
+        <h1>Raspberry Pi - Surveillance Camera</h1>
+        <img src="stram.mjpg" width="640" height="480">
+    </body>
+</html>
