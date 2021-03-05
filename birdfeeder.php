2f244056f9dfde4a2ff364fc0695769191d84ca3
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-03-05 11:57:40 -0500

    changed file location

--- birdfeeder.php
+++ birdfeeder.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>test Raspberry Pi - Surveillance Camera</title>
+        <title>Bird Feeder - Surveillance Camera</title>
     </head>
     <body>
         <h1>Raspberry Pi - Surveillance Camera</h1>
-        <img src="stram.mjpg" width="640" height="480">
+        <iframe src="vids/test.mp4" title="Live Stream"></iframe>
     </body>
 </html>
