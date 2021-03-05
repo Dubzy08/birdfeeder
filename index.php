9024a94cfefb518d2eaad7c79ef1bc6057876457
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-03-05 12:08:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <title>Smart Bird Feeder</title>
     </head>
     <body>
-        <h1>Raspberry Pi - Surveillance Camera</h1>
+        <h1>Bird Feeder Live Stream</h1>
         <iframe width=600 height=350 src="vids/test.mp4" title="Live Stream"></iframe>
     </body>
 </html>
