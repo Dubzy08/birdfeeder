7b188095a9217439530d2064a821e2a8796c56f7
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-03-30 16:51:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,23 @@
     <body>
         <h1>Bird Feeder Live Stream</h1>
         <iframe width=600 height=350 src="vids/test.mp4" title="Live Stream"></iframe>
+        <?php
+            //define variables
+            $year = date("Y");
+            $month = date("m");
+            $day = date("d");
+            $hour = date("H");
+            $minute = date("i");
+
+            //create connection to mysql
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            //check connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            echo "Year:" . $year . " Month:" . $month . " Day:" . $day . " Hour:" . $hour . " Minute:" . $minute;
+        ?>
     </body>
 </html>
