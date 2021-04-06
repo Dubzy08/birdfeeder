fd4b6cd68f8a2c3ca789d8031e88c67e668ade09
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-04-06 14:23:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 
             while($x>0&&$y<30){
 
-                echo '<img src="image/bird.png width="250"';
+                echo '<img src="image/bird.jpg width="250"';
                 echo "<br>";
                 $y++; 
             };
