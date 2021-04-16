520e601835811339327782aafaa920158b3a9da0
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-04-16 10:28:29 -0400

    Update index.php

--- index.php
+++ index.php
@@ -34,9 +34,11 @@
             //select data and query
             $sql = "SELECT * FROM birds";
             $bird = mysqli_query($conn, $sql);
+            $test = mysqli_query($conn, "SELECT MAX(ID) AS ID FROM birds");
             $last_id = mysqli_insert_id($conn);
 
             echo "<br><br>Last inserted ID is:" , mysqli_insert_id($conn) , "<br><br>";
+            echo "<br><br>Last inserted ID is:" , $test , "<br><br>";
 
             echo '<br><br>';
             echo '<h2>Latest Captures of the Feeder</h2>';
