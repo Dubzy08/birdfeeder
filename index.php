c4922c387751861af47ca01b76a6fc3c797d8dc4
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-04-28 20:55:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,62 @@
 <!DOCTYPE html>
+
+<?php
+   define('BASE_DIR', dirname(__FILE__));
+   require_once(BASE_DIR.'/config.php');
+   $config = array();
+   
+   function getImgWidth() {
+      global $config;
+      if($config['vector_preview'])
+         return 'style="width:' . $config['width'] . 'px;"';
+      else
+         return '';
+   }
+   
+   function getLoadClass() {
+      global $config;
+      if(array_key_exists('fullscreen', $config) && $config['fullscreen'] == 1)
+         return 'class="fullscreen" ';
+      else
+         return '';
+   }
+
+   $streamButton = "MJPEG-Stream";
+   $mjpegmode = 0;
+   if(isset($_COOKIE["stream_mode"])) {
+      if($_COOKIE["stream_mode"] == "MJPEG-Stream") {
+         $streamButton = "Default-Stream";
+         $mjpegmode = 1;
+      }
+   }
+   $config = readConfig($config, CONFIG_FILE1);
+   $config = readConfig($config, CONFIG_FILE2);
+   $video_fps = $config['video_fps'];
+   $divider = $config['divider'];
+   $user = getUser();
+   writeLog("Logged in user:" . $user . ":");
+   $userLevel =  getUserLevel($user);
+   writeLog("UserLevel " . $userLevel);
+  ?>
+
 <html>
     <head>
         <title>Smart Bird Feeder</title>
+        <!-- <link rel="stylesheet" href="css/style_minified.css" /> --> <!-- this is for centering the video -->
+        <script src="js/script.js"></script> <!-- this script is used for the live video feed -->
         <style>
             img {
                 border: 4px solid #000;
             }
         </style>
     </head>
-    <body>
+    <body onload="setTimeout('init(<?php echo "$mjpegmode, $video_fps, $divider" ?>);', 100);">
         <h1>Bird Feeder Live Monitor</h1>
-        <iframe width=600 height=350 src="vids/test.mp4" title="Live Stream"></iframe>
+        
+        <div class="container-fluid text-center liveimage">
+            <img id="mjpeg_dest" <?php echo getLoadClass() . getImgWidth();?> src="./loading.jpg"></div>
+        </div>
+
         <?php
             //define variables
             //for sql connection
