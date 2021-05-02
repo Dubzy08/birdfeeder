5e4cb3349673aed4f0f7ed17830325e87b963a2f
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-05-02 17:10:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                 border: 4px solid #000;
             }
             .content {
-                max-width: 50px;
+                max-width: 900px;
                 margin: auto;
             }
         </style>
