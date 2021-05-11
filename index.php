0353bfaf79f4870ba68ea48cbb9b42bdaf309add
Dubzy08 <70661510+Dubzy08@users.noreply.github.com>
2021-05-11 15:56:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -146,7 +146,6 @@
                         if (file_exists($picture)){
                             echo '<img src=' . $picture . ' width="350"';
                             echo "<br>";
-                            echo $picture;
                             $y++;
                         }
                         $id--;
